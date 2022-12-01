545f1fef194b8638bd0447f14f28800824942aca
Chito <Javier.martinez632@gmail.com>
2022-12-01 15:54:06 -0700

    updated fsUtils, notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -8,12 +8,22 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for 
-
+// POST Route for new note 
 notes.post('/', (req, res) => {
+  console.info(req.body);
+  const { noteTitle, noteText } = req.body;
 
+  if(req.body) {
+  const newNote = {
+    noteTitle,
+    noteText, 
+    id: uuid(),
+  };
+  readAndAppend(newNote, 'db/db.json');
+  res.json(`Note added successfully!`);
+  } else {
+    res.error('Error in adding note');
+  }
 });
-
-
 
 module.exports = notes; 
--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const path = require('path');
-const noteData = require('./db/db.json');
+const { custom } = require('./middleware/custom');
 const api = require('./routes/index.js')
-
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// imported custom middleware
+app.use(custom);
+
 // Middleware for parsing JSON urlencoded form data 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('//api', api);
+app.use('/api', api);
+
 // helps give access to all files in public folder  
 app.use(express.static('public'));
-//route to the homepage
+
+//route to the index.html page
 app.get('/', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -22,48 +26,6 @@ app.get('/', (req, res) =>
 app.get('/', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
-// GET api route for the db.json file 
-app.get('/api/notes', (req, res) => {
-  res.json(noteData);
-});
- 
-// POST api route 
-app.post('/api/notes', (req, res) => { 
-  
-  console.info(`${req.method} request received to add a review`);
-
-  const { noteTitle, noteText } = req.body;
-
-  if(noteTitle && noteText) {
-  const newNote = {
-    title,
-    text, 
-    id: uuid(),
-  };
-
-  const noteString = JSON.stringify(newNote);
-
-  // Writes the string to a file
-  fs.writeFile(`./db/${newNote.noteTitle}.json`, noteString, (err) =>
-    err
-      ? console.error(err)
-      : console.log(
-          `Review for ${newNote.noteTitle} has been written to JSON file`
-        )
-  );
-
-  const response = {
-    status: 'success',
-    body: newNote,
-  };
-
-  console.log(response);
-  res.status(201).json(response);
-} else {
-  res.status(500).json('Error in posting review');
-}
-});   
 
 app.listen(PORT, () => 
 console.log(`App lsitening at http://localhost:${PORT}`));
